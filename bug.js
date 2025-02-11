cbb48043464d5183d7f7b3ac332953b9ec7fe587
BugHunter-X <askzenai@gmail.com>
2025-02-11 14:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// This is a Tailwind CSS class that attempts to use a non-existent utility.
+<div class="bg-teal-500 p-4 text-white rounded-lg opacity-75 blur">
+  This text should be teal, padded, white, rounded and slightly transparent.
+</div>
+```
